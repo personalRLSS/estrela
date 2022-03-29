4d37e25822ef5f3135e9d8347fe37bc8be4a9af8
rodrigo <rodrigoluis@gmail.com>
2022-03-29 18:51:11 -0300

    basic update

--- src/house01.js
+++ src/house01.js
@@ -1,52 +1,42 @@
 import * as THREE from  'three';
 import Stats from       '../build/jsm/libs/stats.module.js';
 import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
+import {setMaterial} from './util/util.js'
+import {createFirstFloor} from './firstFloor.js'
 import {initRenderer, 
-        initCamera,
+        initDefaultBasicLight,
         InfoBox,
         onWindowResize} from "../libs/util/util.js";
 
 var stats = new Stats();          // To show FPS information
 var scene = new THREE.Scene();    // Create main scene
 var renderer = initRenderer();    // View function in util/utils
-var camera = initCamera(new THREE.Vector3(0, -30, 15)); // Init camera in this position
+initDefaultBasicLight(scene, true, new THREE.Vector3(25, -20, 30), 100, 1024, 0.1, 200) ;	
+
+// Main camera
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  camera.position.set(7, -10, 3);
+  camera.up.set( 0, 0, 1 );
 
 // Enable mouse rotation, pan, zoom etc.
 var trackballControls = new TrackballControls( camera, renderer.domElement );
+trackballControls.target.set(7,0,3);
 
 // Show axes (parameter is size of each axis)
 var axesHelper = new THREE.AxesHelper( 12 );
 scene.add( axesHelper );
 
-// create the ground plane
-var planeGeometry = new THREE.PlaneGeometry(20, 20);
-planeGeometry.translate(0.0, 0.0, -0.02); // To avoid conflict with the axeshelper
-var planeMaterial = new THREE.MeshBasicMaterial({
-    color: "rgba(150, 150, 150)",
-    side: THREE.DoubleSide,
-});
-var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-// add the plane to the scene
-scene.add(plane);
+//-- First Floor ---------------------------------------------
 
-// create a cube
-var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-var cubeMaterial = new THREE.MeshNormalMaterial();
-var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-// position the cube
-cube.position.set(0.0, 0.0, 2.0);
-// add the cube to the scene
-scene.add(cube);
+let firstFloor = createFirstFloor();
+scene.add(firstFloor);
 
 // Use this to show information onscreen
 var controls = new InfoBox();
   controls.add("Basic Scene");
   controls.addParagraph();
   controls.add("Use mouse to interact:");
-  controls.add("* Left button to rotate");
-  controls.add("* Right button to translate (pan)");
-  controls.add("* Scroll to zoom in/out.");
-  controls.show();
+
 
 // Listen window size changes
 window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
---
+++ src/util/util.js
@@ -0,0 +1,78 @@
+import { MaxEquation } from 'three';
+import * as THREE from '../../../build/three.module.js';
+
+
+// Function to set basic material or textures
+// You can set just a color, just a texture or both
+export function setMaterial(color, file = null, repeatU = 1, repeatV = 1) {
+    let loader = new THREE.TextureLoader();
+    if (!color) color = 'rgb(255,255,255)';
+
+    let mat;
+    if (!file) {
+        mat = new THREE.MeshLambertMaterial({ color: color });
+    } else {
+        mat = new THREE.MeshLambertMaterial({ map: loader.load(file), color: color });
+        mat.map.wrapS = mat.map.wrapT = THREE.RepeatWrapping;
+        mat.map.minFilter = mat.map.magFilter = THREE.LinearFilter;
+        mat.map.repeat.set(repeatU, repeatV);
+    }
+    return mat;
+}
+
+function organizeMaterials(matx, matxneg, maty, matyneg, matz, matzneg)
+{
+   // There is only one material
+   if(!matxneg) return matx;
+
+   // If not, return the full list
+   let newmat = [matx, matxneg, maty, matyneg, matz, matzneg];
+   return newmat;
+}
+
+export function createFourBasicWallRoom(location, 
+                                        sizex, sizey, sizez, 
+                                        posx, posy, posz, 
+                                        material)
+{
+   let room = new THREE.Object3D();
+   // fundo
+   createWallBasic(room, sizex, 0.12, sizez, posx, posy+sizey, posz, material); // fundo
+   // frente
+   createWallBasic(room, sizex, 0.12, sizez, posx, posy, posz, material); // fundo
+   // esquerda
+   createWallBasic(room, 0.12, sizey, sizez, posx, posy, posz, material); // fundo
+   // direita
+   createWallBasic(room, 0.12, sizey, sizez, posx+sizex, posy, posz, material); // fundo 
+   
+   location.add(room);
+}
+
+export function createWallBasic(location, sizeX, sizeY, sizeZ, 
+                                moveX, moveY, moveZ, material)
+{
+   let geometry = new THREE.BoxGeometry(sizeX, sizeY, sizeZ).toNonIndexed();
+   geometry.translate(moveX+sizeX/2.0, moveY+sizeY/2.0, moveZ+sizeZ/2.0); 
+   let wall = new THREE.Mesh(geometry, material);
+   wall.castShadow = true;
+   location.add(wall);
+}
+
+export function createWall(location,
+                           sizeX, sizeY, sizeZ, 
+                           moveX, moveY, moveZ,
+                           matx, matxneg = null,
+                           maty = null, matyneg = null,
+                           matz = null, matzneg = null)
+{
+   let geometry = new THREE.BoxGeometry(sizeX, sizeY, sizeZ).toNonIndexed();
+   geometry.translate(moveX+sizeX/2.0, moveY+sizeY/2.0, moveZ+sizeZ/2.0); // To avoid conflict with the axeshelper
+   let cubeMaterials = organizeMaterials(matx, matxneg, maty, matyneg, matz, matzneg);
+   let wall = new THREE.Mesh(geometry, cubeMaterials);
+      wall.castShadow = true;
+      //wall.receiveShadow = true;
+   
+   location.add(wall);
+}
+
+
