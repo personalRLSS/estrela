f33f1f1c64e80d17d9a07d797a0b526842dd8c85
rodrigoluis <rodrigoluis@gmail.com>
2022-04-03 20:24:41 -0300

    update

--- src/house01.js
+++ src/house01.js
@@ -5,7 +5,8 @@ import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
 import {initRenderer, 
         initDefaultBasicLight,
         InfoBox,
-        onWindowResize} from "../libs/util/util.js";
+        onWindowResize,
+        degreesToRadians} from "../libs/util/util.js";
 import {createFirstFloor} from './firstFloor.js'
 import {createSecondFloor} from './secondFloor.js'
 import {createThirdFloor} from './thirdFloor.js'
@@ -25,7 +26,6 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.xr.enabled = true;
 //renderer.outputEncoding = THREE.sRGBEncoding;
 renderer.shadowMap.enabled = true;
-let moveCamera; // Move when a button is pressed 
 
 window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
 initDefaultBasicLight(scene, true, new THREE.Vector3(25, -20, 30), 100, 1024, 0.1, 200) ;	
@@ -33,10 +33,18 @@ initDefaultBasicLight(scene, true, new THREE.Vector3(25, -20, 30), 100, 1024, 0.
 // Main camera
 var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
   //camera.position.set(7, -10, 3);
-  camera.position.set(21, -12, 12);  
+  camera.position.set(21, -12, 12);    
   camera.up.set( 0, 0, 1 );
 
+  // Enable mouse rotation, pan, zoom etc.
+var trackballControls = new TrackballControls( camera, renderer.domElement );
+   //trackballControls.target.set(7,0,3);
+   trackballControls.target.set(10,4,3);   
+
+
 //-- Create VR button and settings ---------------------------------------------------------------
+let moveCamera; // Move when a button is pressed 
+
 document.body.appendChild( renderer.domElement );
 document.body.appendChild( VRButton.createButton( renderer ) );
 
@@ -46,16 +54,15 @@ var controller1 = renderer.xr.getController( 0 );
 	controller1.addEventListener( 'selectend', onSelectEnd );
 camera.add( controller1 );
 
+// VR Camera
+var cameraVR = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, .1, 1000 );
+
 //-- 'Camera Holder' to help moving the camera
 let cameraHolder = new THREE.Object3D();
-	cameraHolder.position.set(0.0, 1.0, 0.0);
-cameraHolder.add(camera);
+	cameraHolder.position.set(0, -10.0, 1.8);
+   cameraHolder.rotateOnAxis( new THREE.Vector3(1, 0, 0),degreesToRadians(90) )
+cameraHolder.add(cameraVR);
 scene.add( cameraHolder );
-
-// Enable mouse rotation, pan, zoom etc.
-var trackballControls = new TrackballControls( camera, renderer.domElement );
-   //trackballControls.target.set(7,0,3);
-   trackballControls.target.set(10,4,3);   
 
 // Show axes (parameter is size of each axis)
 var axesHelper = new THREE.AxesHelper( 12 );
@@ -83,8 +90,6 @@ var controls = new InfoBox();
 buildInterface();
 animate();
 
-//render();
-
 
 function move()
 {
@@ -92,21 +97,27 @@ function move()
 	{
 		// Get Camera Rotation
 		let quaternion = new THREE.Quaternion();
-		quaternion = camera.quaternion;
+		quaternion = cameraVR.quaternion;
+
+      let a = new THREE.Euler( 1.57, 0, 0, 'XYZ' );
+      let rot = new THREE.Quaternion();
+      rot.setFromEuler(a);
+      quaternion.multiply(rot);
 
 		// Get direction to translate from quaternion
-		var moveTo = new THREE.Vector3(0, 0, -0.1);
+		var moveTo = new THREE.Vector3(0.0, 0.0, 0.1);
 		moveTo.applyQuaternion(quaternion);
 
 		// Move the camera Holder to the computed direction
 		cameraHolder.translateX(moveTo.x);
 		cameraHolder.translateY(moveTo.y);
-		cameraHolder.translateZ(moveTo.z);	
+		cameraHolder.translateZ(moveTo.z);
+      //cameraHolder.rotateOnAxis( new THREE.Vector3(1, 0, 0),degreesToRadians(90) )
+      
+      cameraVR.position.set(moveTo.x,moveTo.y,moveTo.z);	
+     // scene.rotateOnAxis( new THREE.Vector3(1, 0, 0),degreesToRadians(90) )
+
 	}
-   else
-   {
-      trackballControls.update(); // Enable mouse movements
-   }
 }
 
 function onSelectStart( ) 
@@ -136,7 +147,15 @@ function animate()
 function render()
 {
   stats.update(); // Update FPS
-  move();
-  //requestAnimationFrame(render);
-  renderer.render(scene, camera) // Render scene
+
+  if(!renderer.xr.isPresenting)
+  {
+      trackballControls.update(); // Enable mouse movements
+      renderer.render(scene, camera) // Render scene
+  }
+  else
+  {
+     move(); 
+     renderer.render(scene, cameraVR) // Render scene
+  }
 }
