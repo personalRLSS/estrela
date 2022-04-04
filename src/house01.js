9dce28c1069acc4c07f7f705b834faef92aa2fba
rodrigo <rodrigoluis@gmail.com>
2022-04-04 16:27:10 -0300

    update

--- src/house01.js
+++ src/house01.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import Stats from       '../build/jsm/libs/stats.module.js';
 import GUI from '../libs/util/dat.gui.module.js';
 import {TrackballControls} from '../build/jsm/controls/TrackballControls.js';
+import { FlyControls } from '../../build/jsm/controls/FlyControls.js';
 import {initRenderer, 
         initDefaultBasicLight,
         InfoBox,
@@ -14,28 +15,40 @@ import {color} from "./util/settings.js";
 import { setVRMode,
          moveVR} from "./util/VRMode.js";
 
-
 var stats = new Stats();          // To show FPS information
 var scene = new THREE.Scene();    // Create main scene
+let clock = new THREE.Clock();
 var renderer = initRenderer();    // View function in util/utils
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.xr.enabled = true;
 renderer.shadowMap.enabled = true;
 
+let flyMode = true;
+
 window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
 initDefaultBasicLight(scene, true, new THREE.Vector3(25, 30, 20), 100, 1024, 0.1, 200) ;	
 
 // Main camera
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
-  //camera.position.set(7, 3, 10);
+let camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.set(21, 12, 12);    
   camera.up.set( 0, 1, 0 );
 
+// Main camera
+let cameraFly = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
+  cameraFly.position.set(21, 12, 12);    
+  cameraFly.up.set( 0, 1, 0 );
+
   // Enable mouse rotation, pan, zoom etc.
-var trackballControls = new TrackballControls( camera, renderer.domElement );
+let trackballControls = new TrackballControls( camera, renderer.domElement );
    //trackballControls.target.set(7,0,3);
    trackballControls.target.set(10,3,-4);   
+
+let flyCamera = new FlyControls( cameraFly, renderer.domElement );
+   flyCamera.movementSpeed = 5;
+   flyCamera.domElement = renderer.domElement;
+   flyCamera.rollSpeed = 0.2;
+
 
 // Show axes (parameter is size of each axis)
 var axesHelper = new THREE.AxesHelper( 7 );
@@ -71,7 +84,20 @@ animate();
 
 function buildInterface()
 {
+   // Interface
+   var controls = new function ()
+   {
+      this.flyMode = true;
+
+      this.onFlyMode = function(){
+         flyMode = this.flyMode;
+      };
+   }; 
+
   var gui = new GUI();
+  gui.add(controls, 'flyMode', true)
+    .onChange(function() { controls.onFlyMode() })
+    .name("Fly Mode");  
   //gui.add(firstFloor, 'visible', true).name("First Floor");
 //   gui.add(secondFloor, 'visible', true).name("Second Floor");
 //   gui.add(thirdFloor, 'visible', true).name("Third Floor");
@@ -87,10 +113,18 @@ function render()
 {
   stats.update(); // Update FPS
 
-  if(!renderer.xr.isPresenting)
-  {
-      trackballControls.update(); // Enable mouse movements
-      renderer.render(scene, camera) // Render scene
+   if(!renderer.xr.isPresenting)
+   {
+      if(flyMode)
+      {
+         flyCamera.update(clock.getDelta()); 
+         renderer.render(scene, cameraFly) // Render scene
+      }
+      else
+      {
+         trackballControls.update(); // Enable mouse movements
+         renderer.render(scene, camera) // Render scene
+      }
   }
   else
   {
